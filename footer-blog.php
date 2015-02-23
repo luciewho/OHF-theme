6e58495ad69f4028d96fff54fa79d245105e0d40
luciewho <luwuxu@gmail.com>
2015-02-23 10:46:06 -0800

    Create footer-blog.php

---
+++ footer-blog.php
@@ -0,0 +1,51 @@
+<div id="blogfooter">
+
+
+<div id="mailchimpblog">
+<!-- Begin MailChimp Signup Form -->
+<div id="blog-mc_embed_signup" style="margin-bottom: 85px;">
+<form action="http://ohhellofriend.us2.list-manage1.com/subscribe/post?u=6389e17cb3256d13a02240a6b&amp;id=f443130fc1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	
+<div class="mc-field-group" style="width: 290px;">
+		<input type="submit" value="YEA!" name="subscribe" id="blogMC-button" class="button">
+	<div style="overflow: hidden; padding-right: .5em;">
+		<input type="email" value="" name="EMAIL" class="required email" id="blogMC-email" style="float: left;" placeholder="JOIN THE OHF MAILING LIST">
+	</div>
+</form>
+</div>
+<!-- end mailchimp -->
+</div> <!-- end mailchimpblog -->
+<div id="blog-MCmobile">Newsletter Signup: http://eepurl.com/cC2RY</div>
+
+
+<img style="clear:left; float: left; margin-top: -50px;" src="http://ohf.luciewu.com/wp-content/uploads/2014/01/OHFblogfooter.png" width=350 />
+
+<div id="blogfooterlinks">
+<a href="/about">ABOUT</a>
+<a href="/connect">CONNECT</a>
+<a href="/advertise">ADVERTISE</a>
+<a href="/blog">SUBSCRIBE</a>
+<a href="/shop">SHOP</a>
+</div>
+
+<div id="blogfootersocial">
+	<a href="http://facebook.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/facebook.png"></a>
+	<a href="http://instagram.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/instagram.png"></a>
+	<a href="http://pinterest.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/rsz_pinterest.png"></a>
+	<a href="http://twitter.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/twitter.png" /></a>
+</div>
+
+<div id="blogcopyright">
+	<p>Original content &copy; 2008-2014 oh, hello friend <br />
+	We don't mind if you use our photos, please <br />
+	just be sure to credit back to our site properly.</p>
+
+	<p>Site design by oh, hello friend <br />
+	Site development by Lucie Wu</p>
+
+	<p>Thanks for visiting!</p>
+</div>
+</div> <!-- end blogfooter -->
+
+</body> 
+</html> 
