761fc67b243b9bb519b8f35dbb5428b744d55b5b
luciewho <luwuxu@gmail.com>
2015-02-23 10:46:27 -0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,42 @@
+<?php wp_footer(); ?>
+<div id="footer">
+	<div id="footerdiv">
+		
+	
+	<div id="MCsignupbox">
+		<img src="http://ohf.luciewu.com/wp-content/uploads/2014/02/newsletter-tape.png" width=150 style="display: block; margin-top: -25px; margin-left: 45px;" />
+		<img src="http://ohf.luciewu.com/wp-content/uploads/2014/02/newsletter_text.png" style="float: left; margin-right: 15px;"/>
+
+<!-- Begin MailChimp Signup Form --> <div id="mc_embed_signup" style="margin-bottom: 6px; float: left;">
+<form action="http://ohhellofriend.us2.list-manage1.com/subscribe/post?u=6389e17cb3256d13a02240a6b&amp;id=f443130fc1" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	
+<div class="mc-field-group">
+		<input type="submit" value="SUBMIT" name="subscribe" id="mainMC-button" class="button">
+	<div>
+		<input type="email" value="" name="EMAIL" class="required email" id="mainMC-email" placeholder="EMAIL ADDRESS">
+	</div>
+</form>
+</div>
+<!-- end mailchimp -->
+</div>
+</div> <!-- end MCsignupbox -->
+
+
+	<div id="footerlinks">
+		<a href="/contact">Contact</a><br />
+		<a href="/events">Events</a><br />
+	</div><!-- end footerlinks -->
+
+	<div id="footerlinks">
+		<a href="/stockists">Stockists</a><br />
+		<a href="/workshops">Workshops</a>
+	</div><!-- end footerlinks -->
+
+	<img src="http://ohf.luciewu.com/wp-content/uploads/2014/02/footer_mail.png" style="float: left; margin-top: 10px;" />
+	<span id="copymainfooter">&copy; 2014 OH, HELLO FRIEND / OHF STATIONERY PROUDLY PRINTED + MADE IN THE USA</span>
+	</div> <!-- end footerdiv -->
+
+</div> <!-- end footer -->
+</div> <!-- end mainwrapper -->
+</body>
+</html>
