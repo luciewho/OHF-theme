e59327432561a24e0a83b09ba19ae406e9362805
luciewho <luwuxu@gmail.com>
2015-02-23 10:50:32 -0800

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
+    <div>
+        <input type="text" placeholder="SEARCH" name="s" id="blogsearch" />
+    </div>
+</form>
