985a888e9074d5ec40851fc974c2bfad0e6dc2d4
luciewho <luwuxu@gmail.com>
2015-02-23 10:51:07 -0800

    Create sidebar-anchorblog.php

---
+++ sidebar-anchorblog.php
@@ -0,0 +1,82 @@
+<div id="anchor-blogsidebar">
+
+	<div id="socialsidebar">
+	<img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/loved.png" />
+	<a href="http://instagram.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/instagram.png"></a>
+	<a href="http://facebook.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/facebook.png"></a>
+	<a href="http://twitter.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/twitter.png"></a>
+	<a href="http://pinterest.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/rsz_pinterest.png"></a>
+	<a href="http://feeds.feedburner.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/subscribe.png"></a>
+	</div>
+
+	<?php get_search_form(); ?>
+	<img id="washi" src="http://ohf.luciewu.com/wp-content/uploads/2013/12/5_washitape.png" />
+	<img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/blog_youareloved.png" width=170 />
+	<img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/blog_welcomeimage1.png" width=180 style="margin-top: 10px; margin-bottom: 10px;" />
+
+	<hr class="bloghr" />
+	<span class="sponsorheading">SUBSCRIBE/FOLLOW</span>
+	<hr class="bloghr" />
+	<div id="subfollow">
+		<a href="http://feeds.feedburner.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/rss.png"></a>
+		<a href="http://feedburner.google.com/fb/a/mailverify?uri=ohhellofriend&loc=en_US"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/email.png"></a>
+		<a href="https://www.bloglovin.com/blog/2097903/oh-hello-friend-you-are-loved"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/bloglovin.png"></a>
+		<a href="http://feedly.com/index.html#subscription%2Ffeed%2Fhttp%3A%2F%2Fwww.ohhellofriendblog.com%2Ffeeds%2Fposts%2Fdefault"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/feedly.png"></a>
+	</div>
+	
+	<hr class="bloghr" />
+	<span class="sponsorheading">OF INTEREST</span>
+	<hr class="bloghr" />
+	<div class="ofinterestlinks" style="float: left; text-align: left;">
+		<a href="/about">about</a><br />
+		<a href="/coming-soon">shop</a><br />
+		<a href="/connect">contact</a><br />
+		<a href="/events">events</a><br />
+		<a href="/advertise">advertise</a><br />
+	</div>
+	
+	<div class="ofinterestlinks" style="float: right; text-align: right;">
+		<a href="/blogroll">blogroll</a><br />
+		<a href="/link-us">link us</a><br />
+		<a href="/category/notes/">notes</a><br />
+		<a href="/category/trade/">swaps</a><br />
+		<a href="/category/fave/">fave</a><br />
+	</div>
+
+	<hr class="bloghr" />
+	<span class="sponsorheading">MY HEART</span>
+	<hr class="bloghr" />
+	<p id="myheart">christ, rain, truth, nicholas, africa, honesty, travel, vintage, white chocolate, making lists, the number two, design, scarves, family, typography, cardigans, peppermint lip balm, hamburgers, mustard - the color not the condiment :)</p>
+
+	<hr class="bloghr" />
+	<span class="sponsorheading">HAPPENINGS</span>
+	<hr class="bloghr" />
+	<a href="/retail"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/blog_retailshop.png" width=170 /></a>
+
+	<!-- <hr class="bloghr" />
+	<span class="sponsorheading">ENCOURAGE</span>
+	<hr class="bloghr" /> -->
+
+	<hr class="bloghr" />
+	<span class="sponsorheading">LINK US</span>
+	<hr class="bloghr" />
+
+<span style="font-family: Arial, Helvetica, sans-serif;"><a href="http://s305.photobucket.com/albums/nn206/ohhellofriend/blog%20buttons/?action=view&amp;current=110x110.jpg" target="_blank"><img alt="Photobucket" border="0" src="http://i305.photobucket.com/albums/nn206/ohhellofriend/blog%20buttons/110x110.jpg" /></a>&nbsp;</span><br />
+<span style="font-family: Arial, Helvetica, sans-serif; font-size: 8pt;">copy &amp; paste:</span><br />
+<textarea> <a href="http://www.ohhellofriend.com"><img src="http://i305.photobucket.com/albums/nn206/ohhellofriend/blog%20buttons/110x110.jpg" /> </a></textarea>
+	</div><!-- end blogsidebar -->
+
+
+	<div id="anchor-blogsponsors">
+		<hr class="bloghr" />
+		<span class="sponsorheading">SPONSORS</span>
+		<hr class="bloghr" />
+		<a href="/advertise"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/sponsorimage.png" width=170px> </a>
+
+		<hr class="bloghr" />
+		<span class="sponsorheading">INSTAGRAM</span>
+		<hr class="bloghr" />
+
+		<!-- SnapWidget -->
+<iframe src="http://snapwidget.com/in/?u=b2hoZWxsb2ZyaWVuZHxpbnwxNTB8MXwzfHxub3w1fG5vbmV8b25TdGFydHxubw==&v=8114" title="Instagram Widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:155px; height:465px"></iframe>
+	</div> <!-- end blogsponsors -->
