27a983cf92a2e41ce7b2515d48063dc395c9683b
luciewho <luwuxu@gmail.com>
2015-02-23 10:48:49 -0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<?php get_sidebar(); ?>
+
+
+
+
+<div id="contentdiv">
+<div id="content">
+	
+
+		<h1 class="pagetitle"><span class="maintitle"><?php wp_title( $sep ) ?></span> <span class="subtitle"><?php the_subtitle(); ?></span> </h1> 
+	
+	
+	
+	<div id="contentpage">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>  <?php the_content(); ?>  <?php endwhile; endif; ?> 
+	</div>
+
+
+	
+</div> <!-- end content  -->
+</div> <!-- end contentdiv -->
+</div> <!-- end main-->
+	
+<?php get_footer(); ?>
