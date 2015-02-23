b676147a044f3df86eeda632fa5bda987b30cb5a
luciewho <luwuxu@gmail.com>
2015-02-23 10:51:46 -0800

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,22 @@
+<div id="sidebardiv">
+
+<div id="sidebar">
+	<img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/youarelovedgraphic.png" width=200 />
+
+	<div id="sidebarnav">
+		<a style="font-family: Bridone, Futura, Helvetica; font-size: 14pt;" href="/coming-soon">Shop</a>
+		<?php wp_nav_menu( array('menu' => 'Coming Soon' )); ?>
+
+	</div><!-- end sidebarnav -->
+
+<table cellpadding="0" cellspacing="0" id="mainsocial">
+    <tr>
+      <td><a href="http://instagram.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/02/main_instagram.png"></a></td>
+      <td><a href="http://facebook.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/02/main_facebook.png"></a></td>
+      <td><a href="http://pinterest.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/02/main_pinterest.png"></a></td>
+	<td><a href="http://twitter.com/ohhellofriend"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/02/main_twitter.png"></a></td>
+    </tr>
+</table>
+
+</div>
+</div>
