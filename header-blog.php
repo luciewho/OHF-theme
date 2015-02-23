71e71a596d7d3b696289859ed2ab0d1a9b2b484f
luciewho <luwuxu@gmail.com>
2015-02-23 10:47:27 -0800

    Create header-blog.php

---
+++ header-blog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head>
+<title>oh, hello friend | blog</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+<link rel="shortcut icon" href="http://ohf.luciewu.com/wp-content/uploads/2014/02/8_favicon.ico"/>
+
+<?php wp_head(); ?>
+</head>
+
+<body style="background-color: #F2F2F2; min-width: 1200px;">
+<div id="wrapper">
+	<div id="blogheader">
+	
+		<div id="bloglogo"> 
+			<a href="http://ohf.luciewu.com/blog"><img src="http://ohf.luciewu.com/wp-content/uploads/2014/01/OHFblogbanner_final.png" width="650" /></a>
+		
+		<div id="blogheaderlinks">
+			<a href="/category/notes">NOTES</a>
+			<a href="/category/crafts">CRAFTS</a>
+			<a href="/blog">HOME</a>
+			<a href="/category/packaging">PACKAGING</a>
+			<a href="/coming-soon">SHOP</a>
+		</div>
+
+		</div>
+
+	</div>
